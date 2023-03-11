ca48adabcc94104d893e298e1d2bb406da41fc7a
root <anushanagineni123@gmail.com>
2023-03-11 03:10:27 +0000

    techera

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import {Route, Switch, Redirect} from 'react-router-dom'
-import Home from './Components/Home'
-import CourseDetails from './Components/CourseDetails'
-import NotFound from './Components/NotFound'
+import {Switch, Route, Redirect} from 'react-router-dom'
+
+import Home from './components/Home'
+
+import Courses from './components/Courses'
+
+import NotFound from './components/NotFound'
 
 import './App.css'
 
+// Replace your code here
 const App = () => (
   <Switch>
     <Route exact path="/" component={Home} />
-    <Route exact path="/courses/:id" component={CourseDetails} />
+    <Route exact path="/courses/:id" component={Courses} />
     <Route path="/not-found" component={NotFound} />
     <Redirect to="not-found" />
   </Switch>
